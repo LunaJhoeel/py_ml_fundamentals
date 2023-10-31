9ee13c36d35ebef9eddc1a1017083c3229768790
Jhoeel Luna <lunajhoeel@gmail.com>
2023-10-30 22:43:40 -0500

    feat

---
+++ cap_01_ambiente/scripts/hola_mundo.py
@@ -0,0 +1,12 @@
+# Importar la función ic de icecream
+from icecream import ic
+
+# Utilizar ic para imprimir el mensaje "Hola, Cibertec!"
+ic("Hola, Cibertec!")
+
+# Definir una cadena de texto
+mensaje = "Hola, Cibertec!"
+
+print(mensaje)
+# Utilizar ic para imprimir la cadena con una descripción personalizada
+ic(mensaje)
---
+++ cap_01_ambiente/scripts/setup.md
@@ -0,0 +1,11 @@
+# Creación de un entorno venv para scripts
+python3.9 -m venv venv
+
+## Activación de ambiente virtual
+source venv/bin/activate
+
+## Instalación de dependencias
+pip install -r requirements.txt
+
+## Ejecutar script
+python hola_mundo.py
